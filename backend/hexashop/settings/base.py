f1270054c2e425abb821f7bf05dbc0e8de2774ee
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-05-11 17:50:31 +0600

    Update backend

--- backend/hexashop/settings/base.py
+++ backend/hexashop/settings/base.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sitemaps",
     # Third-party
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "django_filters",
     "cloudinary",
